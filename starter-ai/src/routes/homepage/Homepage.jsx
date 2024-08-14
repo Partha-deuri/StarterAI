f0ebb1aea29eab85387f3fad8ad4112bc1d2ce65
Partha-deuri <parthapratimdeuri4@gmail.com>
2024-08-14 15:38:13 +0530

    landing page done

--- starter-ai/src/routes/homepage/Homepage.jsx
+++ starter-ai/src/routes/homepage/Homepage.jsx
@@ -4,7 +4,25 @@ import "./homepage.css";
 const Homepage = () => {
     return (
         <div className="homepage">
-            <Link to={"/dashboard"}>DashBoard</Link>
+            <img src="/orbital.png" className="orbital" alt="" />
+            <div className="left">
+                <h1>STARTER AI</h1>
+                <h2>Make things easy for you</h2>
+                <h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+                    ipsa, esse earum iusto facilis cumque corporis sit
+                    repellendus incidunt unde!
+                </h3>
+                <Link to={"/dashboard"}>Get Started</Link>
+            </div>
+            <div className="right">
+                <div className="imgContainer">
+                    <div className="bgContainer">
+                        <div className="bg"></div>
+                    </div>
+                    <img src="/bot.png" className="bot" />
+                </div>
+            </div>
         </div>
     );
 };
